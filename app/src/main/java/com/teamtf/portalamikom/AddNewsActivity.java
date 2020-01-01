01acc822a29511787c7e56fd1b6bd9fa4f236753
Widi Riawan Manolija <qlueqlass@gmail.com>
2020-01-01 22:52:27 +0700

    Update Commit

--- app/src/main/java/com/teamtf/portalamikom/AddNewsActivity.java
+++ app/src/main/java/com/teamtf/portalamikom/AddNewsActivity.java
@@ -1,20 +1,31 @@
 package com.teamtf.portalamikom;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AddNewsActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_news);
 
-        ActionBar toolbar = getSupportActionBar();
-        toolbar.setDisplayHomeAsUpEnabled(true);
-        toolbar.setDisplayShowHomeEnabled(true);
-        toolbar.setTitle("Tambah Berita");
+        toolbar = findViewById(R.id.toolbar_add_news);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
--- app/src/main/java/com/teamtf/portalamikom/addEventActivity.java
+++ app/src/main/java/com/teamtf/portalamikom/addEventActivity.java
@@ -1,20 +1,31 @@
 package com.teamtf.portalamikom;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AddEventActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
 
-        ActionBar toolbar = getSupportActionBar();
-        toolbar.setDisplayHomeAsUpEnabled(true);
-        toolbar.setDisplayShowHomeEnabled(true);
-        toolbar.setTitle("Tambah Event");
+        toolbar = findViewById(R.id.toolbar_add_event);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
