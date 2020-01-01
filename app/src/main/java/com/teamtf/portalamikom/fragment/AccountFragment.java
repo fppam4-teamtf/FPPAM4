33fcdd2fd0f1585b2018c9870316b6e86d2ce9d2
Widi Riawan Manolija <qlueqlass@gmail.com>
2020-01-01 18:36:36 +0700

    Update Commit

--- app/src/main/java/com/teamtf/portalamikom/fragment/AccountFragment.java
+++ app/src/main/java/com/teamtf/portalamikom/fragment/AccountFragment.java
@@ -5,11 +5,13 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -21,6 +23,8 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.teamtf.portalamikom.AddEventActivity;
+import com.teamtf.portalamikom.AddNewsActivity;
 import com.teamtf.portalamikom.MainActivity;
 import com.teamtf.portalamikom.R;
 
@@ -30,12 +34,8 @@ import com.teamtf.portalamikom.R;
 public class AccountFragment extends Fragment{
 
     private SharedPreferences prefs;
-    private RelativeLayout relativeLayout;
-    private Context context;
 
     private MainActivity main;
-
-    private TextView tvUserId, tvUserName, tvGender, tvAddress;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -57,14 +57,14 @@ public class AccountFragment extends Fragment{
 
         prefs = getContext().getSharedPreferences("login", Context.MODE_PRIVATE);
 
-        relativeLayout = v.findViewById(R.id.rl_fmt_account);
+        RelativeLayout relativeLayout = v.findViewById(R.id.rl_fmt_account);
 
         main = (MainActivity) getActivity();
 
-        tvUserId = v.findViewById(R.id.tv_userid);
-        tvUserName = v.findViewById(R.id.tv_nama);
-//        tvGender = v.findViewById(R.id.tv_gender);
-//        tvAddress = v.findViewById(R.id.tv_address);
+        TextView tvUserId = v.findViewById(R.id.tv_userid);
+        TextView tvUserName = v.findViewById(R.id.tv_nama);
+//        TextView tvGender = v.findViewById(R.id.tv_gender);
+//        TextView tvAddress = v.findViewById(R.id.tv_address);
 
         if (!prefs.getAll().isEmpty()){
             tvUserId.setText(prefs.getString("userid","value"));
@@ -72,6 +72,24 @@ public class AccountFragment extends Fragment{
 //            tvGender.setText(prefs.getString("gender","value"));
 //            tvAddress.setText(prefs.getString("address","value"));
         }
+
+        CardView cvAddNews = v.findViewById(R.id.cv_add_news);
+        cvAddNews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), AddNewsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        CardView cvAddEvent = v.findViewById(R.id.cv_add_event);
+        cvAddEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), AddEventActivity.class);
+                startActivity(i);
+            }
+        });
 
         Button btnLogout = v.findViewById(R.id.btn_logout);
         btnLogout.setOnClickListener(new View.OnClickListener() {
@@ -115,7 +133,6 @@ public class AccountFragment extends Fragment{
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
-        this.context = context;
     }
 
 }
